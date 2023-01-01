ac4c936507b8773419b709c09c0ca5c9eec273ee
Daiita30 <davidcristianps675@gmail.com>
2023-01-01 23:17:56 +0700

    kedua

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const fireDiv = document.querySelector(".fireworksDiv");
 const fireworks = new Fireworks(fireDiv, {
-  delay: { min: 10, max: 15 },
+  delay: { min: 25, max: 50 },
   trace: 5,
   speed: 0.5,
-  particles: 200,
+  particles: 150,
   sound: {
     enable: true,
     files: ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"],
